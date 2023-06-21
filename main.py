0e78e1e6f2f78105197ac7fb259f7f5804dbc672
TzzzzzT <m.tomo9838@gmail.com>
2023-06-21 22:09:44 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.title('streamlit 超入門')
+
+st.write('ブログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i +1)
+
+
+
+left_column, right_column = st.columns(2)
+left_column.button('右カラムに文字を表示')
+
+    
+expander1 = st.expander('問い合わせ1')
+expander1.write('問い合わせ1の回答')
+expander2 = st.expander('問い合わせ2')
+expander2.write('問い合わせ2の回答')
+expander3 = st.expander('問い合わせ3')
+expander3.write('問い合わせ3の回答')
+
